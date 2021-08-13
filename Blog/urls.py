4d6fb8f236bff52d90312930cbe4ab354fcd2425
Siddhant Maurya <mauryasiddhant758@gmail.com>
2021-08-14 00:35:11 +0530

    make it better

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,6 +3,7 @@ from Blog import views
 from django.conf.urls.static import static
 from django.conf import settings
 from .views import BlogDetailView
+from django.views.static import serve
 
 urlpatterns = [
     
@@ -13,5 +14,7 @@ urlpatterns = [
     path('addblogs/',views.Add_blogs),
     path('logout/',views.Logout),
     path('search/',views.search),
+    path(r'^media/(?P<path>.*)$' ,serve,{'document_root':settings.MEDIA_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
